55e1a2e9c2902b3df899eedf6a29a5b4efb98819
Steve Traylen <steve.traylen@cern.ch>
2024-07-08 16:21:29 +0200

    Create quadlets directory when needed
    On Archlinux in particular the directory `/etc/containers/systemd` does
    not exist. Via an optional parameter and hiera settings create the
    directory on Archlinux.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -14,6 +14,13 @@ describe 'quadlets' do
         it { is_expected.to contain_package('podman').with_ensure('installed') }
         it { is_expected.to contain_service('podman.socket').with_ensure(true).with_enable(true) }
 
+        case facts[:os]['family']
+        when 'Archlinux'
+          it { is_expected.to contain_file('/etc/containers/systemd').with_ensure('directory') }
+        else
+          it { is_expected.not_to contain_file('/etc/containers/systemd') }
+        end
+
         context 'with service enabled' do
           let(:params) do
             { socket_enable: true }
