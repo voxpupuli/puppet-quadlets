2c3078b6b46efccb9e82b4a0831772c73eae4f27
Tim Meusel <tim@bastelfreak.de>
2024-09-10 07:16:00 +0200

    FacterDB: Switch to stringified keys

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 
 describe 'quadlets' do
   context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
+    on_supported_os.each do |os, os_facts|
       context "on #{os}" do
-        let(:facts) { facts }
+        let(:facts) { os_facts }
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_class('quadlets') }
@@ -14,7 +14,7 @@ describe 'quadlets' do
         it { is_expected.to contain_package('podman').with_ensure('installed') }
         it { is_expected.to contain_service('podman.socket').with_ensure(true).with_enable(true) }
 
-        case facts[:os]['family']
+        case os_facts['os']['family']
         when 'Archlinux'
           it { is_expected.to contain_file('/etc/containers/systemd').with_ensure('directory') }
         else
