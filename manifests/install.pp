62cd17fe9f7aea535522fb09e04fcb7f7903f8fc
Jo Rhett <geek@jorhett.com>
2025-04-17 08:34:17 -0700

    Make podman package management optional

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,12 @@
 # @summary Install podman software for quadlet support
 # @api private
 #
-class quadlets::install {
-  package { 'podman':
-    ensure => installed,
+class quadlets::install (
+  Boolean $manage_package = $quadlets::manage_package,
+) {
+  if $manage_package {
+    package { 'podman':
+      ensure => installed,
+    }
   }
 }
