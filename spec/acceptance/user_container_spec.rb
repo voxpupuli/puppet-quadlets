63103137b6e99d91e36b7905b4dc3bc47e2e191f
Sander Hoentjen <shoentjen@antagonist.nl>
2025-08-12 19:38:36 +0200

    Add acceptance test with 2 user containers

--- spec/acceptance/user_container_spec.rb
+++ spec/acceptance/user_container_spec.rb
@@ -91,4 +91,125 @@ describe 'quadlets::quadlet' do
       end
     end
   end
+
+  context 'with 2 simple CentOS user container running' do
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+
+        # We might want to fall back on fuse-overlayfs
+        # rather than rely on overlay working.
+        #
+        package{'fuse-overlayfs':
+          ensure => present,
+          before => [Quadlets::Quadlet['centos-user1.container'],Quadlets::Quadlet['centos-user2.container']],
+        }
+        # systemd-logind is masked on almalinux, enable it because it is needed
+        # for user containers
+        service{'systemd-logind.service':
+          ensure => true,
+          enable => true,
+        }
+        # begin hacks to make it work on rootless in rootless container
+        file_line{'contoiners_subgid':
+          path   => '/etc/subgid',
+          line   => 'containers:10000:5000',
+          before => User['containers'],
+        }
+        file_line{'contoiners_subuid':
+          path   => '/etc/subuid',
+          line   => 'containers:10000:5000',
+          before => User['containers'],
+        }
+        exec{'setcap_newgidmap':
+          command => '/usr/sbin/setcap cap_setgid=ep /usr/bin/newgidmap',
+          unless  => '/usr/sbin/getcap /usr/bin/newgidmap | grep -q cap_setgid=ep',
+          before  => User['containers'],
+        }
+        exec{'setcap_newuidmap':
+          command => '/usr/sbin/setcap cap_setuid=ep /usr/bin/newuidmap',
+          unless  => '/usr/sbin/getcap /usr/bin/newuidmap | grep -q cap_setuid=ep',
+          before  => User['containers'],
+        }
+        # end hacks to make it work on rootless in rootless container
+        user{'containers':
+          ensure     => present,
+          managehome => true,
+        }
+        loginctl_user{'containers':
+          linger  => enabled,
+        }
+        file{['/home/containers/.config', '/home/containers/.config/containers', '/home/containers/.config/containers/systemd']:
+          ensure => directory,
+          owner  => 'containers',
+          group  => 'containers',
+        }
+        quadlets::quadlet{'centos-user1.container':
+          ensure          => present,
+          user            => {
+            'name' => 'containers',
+          },
+          unit_entry      => {
+           'Description' => 'Trivial First Container that will be very lazy',
+          },
+          service_entry   => {
+            'TimeoutStartSec' => '900',
+          },
+          container_entry => {
+            'Image'  => 'quay.io/centos/centos:latest',
+            'Exec'   => 'sh -c "sleep inf"',
+          },
+          install_entry   => {
+            'WantedBy' => 'default.target',
+          },
+          active          => true,
+        }
+        quadlets::quadlet{'centos-user2.container':
+          ensure          => present,
+          user            => {
+            'name' => 'containers',
+          },
+          unit_entry      => {
+           'Description' => 'Trivial Second Container that will be very lazy',
+          },
+          service_entry   => {
+            'TimeoutStartSec' => '900',
+          },
+          container_entry => {
+            'Image'  => 'quay.io/centos/centos:latest',
+            'Exec'   => 'sh -c "sleep inf"',
+          },
+          install_entry   => {
+            'WantedBy' => 'default.target',
+          },
+          active          => true,
+        }
+        PUPPET
+      end
+    end
+
+    describe 'centos.service user1 unit' do
+      it 'is running' do
+        result = command('systemctl --user --machine containers@ is-active centos-user1.service')
+        expect(result.stdout.strip).to eq('active')
+      end
+
+      it 'is enabled' do
+        result = command('systemctl --user --machine containers@ is-enabled centos-user1.service')
+        expect(result.stdout.strip).to eq('generated')
+      end
+    end
+
+    describe 'centos.service user2 unit' do
+      it 'is running' do
+        result = command('systemctl --user --machine containers@ is-active centos-user2.service')
+        expect(result.stdout.strip).to eq('active')
+      end
+
+      it 'is enabled' do
+        result = command('systemctl --user --machine containers@ is-enabled centos-user2.service')
+        expect(result.stdout.strip).to eq('generated')
+      end
+    end
+  end
 end
