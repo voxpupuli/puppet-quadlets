6c1602742923a11d3f7b3e511c340f60de0980a0
Steve Traylen <steve.traylen@cern.ch>
2025-10-15 12:55:46 +0200

    Allow Pods with PublishPort = 8080:80
    While containers and kubes both allowed a `PublishPort` of
    `hostPort:containerPort` pods did not.
    
    * https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html#id15

--- spec/type_aliases/unit_pod_spec.rb
+++ spec/type_aliases/unit_pod_spec.rb
@@ -8,4 +8,6 @@ describe 'Quadlets::Unit::Pod' do
   it { is_expected.to allow_value({ 'Hostname' => 'foo.example.net' }) }
   it { is_expected.to allow_value({ 'Label' => %w[abc xyz] }) }
   it { is_expected.to allow_value({ 'Label' => 'xyz' }) }
+  it { is_expected.to allow_value({ 'PublishPort' => ['1234:5678'] }) }
+  it { is_expected.not_to allow_value({ 'PublishPort' => '1234:5678' }) }
 end
