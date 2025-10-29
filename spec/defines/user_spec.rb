e0bcf197f23f7eaebbf8fe1d48ee089a4e8d4b4f
Steve Traylen <steve.traylen@cern.ch>
2025-10-29 21:45:31 +0100

    Add tests for `quadlet::user`
    The `quadlet::user` type was missing any tests

---
+++ spec/defines/user_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'quadlets::user' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'with a simple user image' do
+        let(:title) { 'nano' }
+        let(:params) do
+          {
+            user: {
+              'name' => 'nano',
+            }
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_user('nano') }
+        it { is_expected.to contain_user('nano').with_foo }
+        it { is_expected.to contain_loginctl_user('nano').with_linger('enabled') }
+      end
+
+      context 'with a simple user actions disabled' do
+        let(:title) { 'micro' }
+        let(:params) do
+          {
+            user: {
+              'name'          => 'micro',
+              'manage_linger' => false,
+              'manage_user'   => false,
+              'create_dir'    => false,
+            }
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.not_to contain_user('micro') }
+        it { is_expected.not_to contain_loginctl_user('micro') }
+      end
+    end
+  end
+end
