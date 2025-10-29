af335e5c901a6988649c04dcd19296aaf9d57720
Steve Traylen <steve.traylen@cern.ch>
2025-10-29 22:09:30 +0100

    Explicitly set the group of quadlet user
    While the group ownership of the quadlet files was being specified
    the group of the user was not being specified.

--- spec/defines/user_spec.rb
+++ spec/defines/user_spec.rb
@@ -7,7 +7,7 @@ describe 'quadlets::user' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      context 'with a simple user image' do
+      context 'with a simple user' do
         let(:title) { 'nano' }
         let(:params) do
           {
@@ -19,8 +19,23 @@ describe 'quadlets::user' do
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_user('nano') }
-        it { is_expected.to contain_user('nano').with_foo }
         it { is_expected.to contain_loginctl_user('nano').with_linger('enabled') }
+      end
+
+      context 'with a simple user and group' do
+        let(:title) { 'pico' }
+        let(:params) do
+          {
+            user: {
+              'name'  => 'pico',
+              'group' => 'giga',
+            }
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_user('pico').with_gid('giga') }
+        it { is_expected.to contain_loginctl_user('pico').with_linger('enabled') }
       end
 
       context 'with a simple user actions disabled' do
