9b0144d478abd3d0c37343c14e2be33caf7b6246
Ed Rude <ed.rude@gmail.com>
2025-11-25 11:33:02 -0500

    allow scaler or array instead of requiring array

--- spec/type_aliases/unit_container_spec.rb
+++ spec/type_aliases/unit_container_spec.rb
@@ -8,7 +8,7 @@ describe 'Quadlets::Unit::Container' do
   it { is_expected.to allow_value({ 'PublishPort' => ['1234-12346'] }) }
   it { is_expected.to allow_value({ 'PublishPort' => [1234, '123.111.1.1:55:72'] }) }
   it { is_expected.to allow_value({ 'PublishPort' => ['1234:5678'] }) }
-  it { is_expected.not_to allow_value({ 'PublishPort' => '1234:5678' }) }
+  it { is_expected.to allow_value({ 'PublishPort' => '1234:5678' }) }
   it { is_expected.to allow_value({ 'Entrypoint' => 'python3' }) }
   it { is_expected.to allow_value({ 'Entrypoint' => '["/usr/bin/sleep", "inf"]' }) }
   it { is_expected.to allow_value({ 'Exec'  => '/bin/bash' }) }
