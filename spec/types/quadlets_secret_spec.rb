81982b29bc8faddf64f5b5c2fd9040619a073fe8
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2025-12-01 22:15:04 +0100

    add spec test for quadlets_secret type

---
+++ spec/types/quadlets_secret_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'quadlets_secret' do
+      let(:title) { 'root:asecret' }
+
+      context 'with default provider' do
+        it { is_expected.to be_valid_type.with_properties('ensure') }
+        it { is_expected.to be_valid_type.with_parameters('driver') }
+        it { is_expected.to be_valid_type.with_parameters('doptions') }
+        it { is_expected.to be_valid_type.with_properties('labels') }
+        it { is_expected.to be_valid_type.with_properties('secret') }
+      end
+    end
+
