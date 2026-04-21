bc80f2a42678f5294b44b00090aa9bc2202a344b
Pat Riehecky <riehecky@fnal.gov>
2026-04-21 10:27:41 -0500

    Add missing Container and Pod quadlet keys; expand spec coverage
    Container: add AppArmor, HttpProxy, LogOpt, HealthStartupRetries,
    HealthStartupSuccess, RetryDelay, SecurityLabelLevel, SecurityLabelType;
    fix PidsLimits -> PidsLimit (matches quadlet man page spelling).
    
    Pod: add AddHost, DNS, DNSOption, DNSSearch, ExitPolicy, GIDMap, IP, IP6,
    NetworkAlias, ServiceName, ShmSize, StopTimeout, SubGIDMap, SubUIDMap, UIDMap.
    
    Spec: expand type alias tests to cover every defined key across Container,
    Pod, Kube, Network, Volume, and Image unit types.

--- spec/type_aliases/unit_kube_spec.rb
+++ spec/type_aliases/unit_kube_spec.rb
@@ -7,10 +7,16 @@ describe 'Quadlets::Unit::Kube' do
   it { is_expected.to allow_value({ 'AutoUpdate' => %w[registry image] }) }
   it { is_expected.to allow_value({ 'ConfigMap' => '/path/to/configmap.yaml' }) }
   it { is_expected.to allow_value({ 'ConfigMap' => ['/path/to/configmap.yaml'] }) }
+  it { is_expected.to allow_value({ 'ContainersConfModule' => '/etc/nvd.conf' }) }
+  it { is_expected.to allow_value({ 'ContainersConfModule' => ['/etc/nvd.conf', '/etc/extra.conf'] }) }
+  it { is_expected.not_to allow_value({ 'ExitCodePropagation' => 'random' }) }
+  it { is_expected.to allow_value({ 'ExitCodePropagation' => 'all' }) }
   it { is_expected.to allow_value({ 'ExitCodePropagation' => 'any' }) }
+  it { is_expected.to allow_value({ 'ExitCodePropagation' => 'none' }) }
   it { is_expected.to allow_value({ 'GlobalArgs' => '--log-level=info' }) }
   it { is_expected.to allow_value({ 'GlobalArgs' => ['--log-level=info', '--cgroup-manager=systemd'] }) }
   it { is_expected.to allow_value({ 'KubeDownForce' => true }) }
+  it { is_expected.to allow_value({ 'LogDriver' => 'journald' }) }
   it { is_expected.to allow_value({ 'Network' => 'host' }) }
   it { is_expected.to allow_value({ 'Network' => ['host'] }) }
   it { is_expected.to allow_value({ 'PodmanArgs' => '--pull=newer' }) }
@@ -19,5 +25,9 @@ describe 'Quadlets::Unit::Kube' do
   it { is_expected.to allow_value({ 'PublishPort' => ['1234-12346'] }) }
   it { is_expected.to allow_value({ 'PublishPort' => '1234:5678' }) }
   it { is_expected.to allow_value({ 'PublishPort' => ['1234:5678'] }) }
+  it { is_expected.not_to allow_value({ 'SetWorkingDirectory' => 'random' }) }
+  it { is_expected.to allow_value({ 'SetWorkingDirectory' => 'yaml' }) }
+  it { is_expected.to allow_value({ 'SetWorkingDirectory' => 'unit' }) }
+  it { is_expected.to allow_value({ 'UserNS' => 'auto' }) }
   it { is_expected.to allow_value({ 'Yaml' => '/path/to/yaml/file' }) }
 end
--- spec/type_aliases/unit_network_spec.rb
+++ spec/type_aliases/unit_network_spec.rb
@@ -4,19 +4,26 @@ require 'spec_helper'
 
 describe 'Quadlets::Unit::Network' do
   it { is_expected.to allow_value({ 'ContainersConfModule' => '/etc/container/1.conf' }) }
+  it { is_expected.to allow_value({ 'ContainersConfModule' => ['/etc/container/1.conf', '/etc/container/2.conf'] }) }
   it { is_expected.to allow_value({ 'DisableDNS'           => true }) }
+  it { is_expected.to allow_value({ 'DisableDNS'           => false }) }
   it { is_expected.to allow_value({ 'DNS'                  => '192.0.2.1' }) }
   it { is_expected.to allow_value({ 'Driver'               => 'bridge' }) }
+  it { is_expected.to allow_value({ 'Driver'               => 'macvlan' }) }
   it { is_expected.to allow_value({ 'Gateway'              => '192.0.2.2' }) }
   it { is_expected.to allow_value({ 'GlobalArgs'           => '--log-level=debug' }) }
+  it { is_expected.to allow_value({ 'GlobalArgs'           => ['--log-level=debug', '--cgroup-manager=systemd'] }) }
   it { is_expected.to allow_value({ 'Internal'             => true }) }
   it { is_expected.to allow_value({ 'IPAMDriver'           => 'dhcp' }) }
+  it { is_expected.to allow_value({ 'IPAMDriver'           => 'host-local' }) }
   it { is_expected.to allow_value({ 'IPRange'              => '192.0.2.0/25' }) }
   it { is_expected.to allow_value({ 'IPv6'                 => true }) }
   it { is_expected.to allow_value({ 'Label'                => 'XYZ' }) }
+  it { is_expected.to allow_value({ 'Label'                => %w[XYZ ABC] }) }
   it { is_expected.to allow_value({ 'NetworkDeleteOnStop'  => true }) }
   it { is_expected.to allow_value({ 'NetworkName'          => 'foo' }) }
   it { is_expected.to allow_value({ 'Options'              => 'isolate=true' }) }
   it { is_expected.to allow_value({ 'PodmanArgs'           => '--dns=192.0.2.1' }) }
+  it { is_expected.to allow_value({ 'PodmanArgs'           => ['--dns=192.0.2.1', '--log-level=debug'] }) }
   it { is_expected.to allow_value({ 'Subnet'               => '192.0.2.0/24' }) }
 end
