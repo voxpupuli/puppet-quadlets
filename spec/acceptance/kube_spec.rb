ce55fbff352cc7271d3aa24c38de2b5e508ed9a5
Steve Traylen <steve.traylen@cern.ch>
2026-09-15 11:29:28 +0200

    Stop setting runroot and graphroot
    On very new podman (CentOS Stream 10, ArchLinux ) the setting for
    runroot and graphroot in the system containers file are no longer
    ignored for rootless podman.
    
    Letting the defaults play out is fine. The important bit is choosing
    overlay for Github CI context

--- spec/acceptance/kube_spec.rb
+++ spec/acceptance/kube_spec.rb
@@ -7,21 +7,35 @@ describe 'quadlets::quadlet' do
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
         <<-PUPPET
-        file { ['/etc/containers/', '/etc/containers/yaml/']:
-          ensure => 'directory',
-        }
-        file { '/etc/containers/storage.conf':
-          ensure  => 'file',
-          content => @(CONF),
+         file { ['/etc/containers/', '/etc/containers/yaml/']:
+           ensure => 'directory',
+         }
+         # This works for Debian 13 but future debians may well
+         # look more like the default case.
+         $_storage_conf = $facts['os']['family'] ? {
+           'Debian' => @(CONF_DEBIAN),
+             [storage]
+             driver = "overlay"
+             runroot = "/var/lib/containers/storage"
+             graphroot = "/var/lib/containers/storage"
+
+             [storage.options.overlay]
+             mount_program = "/usr/bin/fuse-overlayfs"
+             | CONF_DEBIAN
+           default  => @(CONF),
             [storage]
             driver = "overlay"
-            runroot = "/var/lib/containers/storage"
-            graphroot = "/var/lib/containers/storage"
 
             [storage.options.overlay]
             mount_program = "/usr/bin/fuse-overlayfs"
             | CONF
         }
+
+        file { '/etc/containers/storage.conf':
+          ensure  => 'file',
+          content => $_storage_conf,
+        }
+
         file { '/etc/containers/yaml/pod.yaml':
           content => @(POD),
             apiVersion: v1
